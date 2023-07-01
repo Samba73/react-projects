a52cfc37171d8468e1104482040fd359f576c18e
sambak73 <raghuisnow@yahoo.com>
2023-07-01 15:05:04 +0530

    completed colorpattern without css styling

---
+++ colorpattern/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+
+  const [Color, setColor] = React.useState('')
+  const [Hexvalue, setHexvalue] = React.useState('')
+  const [Textvalue, setTextvalue] = React.useState('black')
+
+  const colorpattern = 
+    {
+      red: '#FF0000',
+      green: '#008000',
+      blue: '#0000FF',
+      black: '#000000',
+      white: '#FFFFFF',
+    }
+
+  const handleChange = (e) => {
+    setColor(e.target.value)
+    //console.log('event value', e.target.value)
+    //console.log('set Color state', Color)
+  }
+
+  const handleColor = () => {
+    console.log(Color)
+    console.log(colorpattern[Color])
+    setHexvalue(colorpattern[Color])
+  }
+
+  const handleText = () => {
+    Textvalue === 'black' ? setTextvalue('white') : setTextvalue('black')
+  }
+  return (
+    <div className="App">
+      <p>
+      <textarea
+        style = {{backgroundColor: Hexvalue, color: Textvalue}}
+        value = {Color}
+      /></p>
+      <p>
+      <input
+        type = "text"
+        placeholder = "Enter color"
+        onChange={(e) => {handleChange(e)}}
+      /></p>
+      <p><button onClick={()=>handleColor()}>Change Color</button></p>
+      <p><button onClick={()=>handleText()}>Change Text</button></p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ToDoAdd.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import Items from './Items';
+import { CgAddR } from 'react-icons/cg';
+import './Content.css'
+
+
+const ToDoAdd = ({todoCount, allItems}) => {
+    // const [todo, settodo] = React.useState('')
+    // const [todos, settodos] = React.useState([])
+    const [todocount, settodocount] = React.useState(todoCount)
+    const [items, setItems] = React.useState(allItems)
+    const [todo, settodo] = React.useState('')
+
+    //console.log('todoCount from App', todoCount)
+    //console.log('items from App', allItems)
+function handleAddItem(){
+    if (!todo) return
+    const listItems = [{
+        id: todocount+1,
+        checked: false,
+        title: todo
+    }]
+    //console.log(listItems)
+    const updatedItems = [...items, ...listItems]
+    setItems(updatedItems)
+    localStorage.setItem('todo_list',JSON.stringify(updatedItems))
+    //console.log(items)
+    settodocount(+todocount+1)
+    settodo('')
+}  
+
+function handleItemEntry(event){
+    settodo(event.target.value)
+    //console.log(event)
+    //event.target.value = ""
+    
+}
+function handleRemoveItem(id){
+    //alert(id)
+    let listItems = items.filter(item => item.id !== id)
+    setItems(listItems)
+    localStorage.setItem('todo_list', JSON.stringify(listItems))
+}
+function handleSelection(id) {
+    const listItems = items.map((item) => 
+        item.id === id ? {...item, ischecked:!item.ischecked} : item
+    )
+    setItems(listItems)
+    localStorage.setItem('todo_list', JSON.stringify(listItems))
+}
+  return (
+        <main className='main'>
+        <p>Welcome to ToDo App. You have total {todocount} ToDo's</p>
+        <div>
+            <ul>
+                <li className="new">
+                    <input type='text' placeholder='Add ToDo Item' value = {todo} onChange={(event) => handleItemEntry(event)}/>
+                    <button onClick={handleAddItem}><CgAddR role="button" tabIndex="0"/></button>
+                </li>
+            </ul>
+        </div>
+        <p>
+            <h2>
+                Available ToDo's are...
+            </h2>
+            {(items && items.length > 0) ? 
+            (
+                <Items 
+                    items = {items}
+                    handleSelection = {handleSelection}
+                    handleRemoveItem = {handleRemoveItem}
+
+                />
+            ): <p><h2>Your ToDo list is empty</h2></p>}         
+        </p>
+        </main>
+  )
+}
+
+export default ToDoAdd
